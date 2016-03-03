09b0659f55845741cf2fb81aea439314d2b6760e
Megan Salisbury <megan.a.salisbury@gmail.com>
2016-03-03 10:33:56 -0500

    Final

--- local_events.rb
+++ local_events.rb
@@ -1,1 +1,14 @@
 # your code, here.
+require 'json'
+require 'httparty'
+
+response = HTTParty.get('https://api.seatgeek.com/2/events?venue.state=MA&datetime_utc.gt=2016-02-01')
+
+data = JSON.parse(response.body)["events"]
+
+# puts data
+
+# puts JSON.generate(data)
+data.each do |event|
+  puts "#{event["title"]} @ #{event["venue"]["name"]}"
+end
